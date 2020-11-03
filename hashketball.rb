287d1611947bc403bc865cd9140a5dc20b75b8cf
Mai Pham <maitphm@gmail.com>
2020-11-03 01:08:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,9 +195,13 @@ def big_shoe_rebounds
   game_hash.each do |team, value|
     value[:players].each do |stat| 
       if shoe_size < stat[:shoe]
-        shoe_size 
-        
-      
+        shoe_size = stat[:shoe]
+        rebounds = stat[:rebounds]
+      end 
+    end 
+  end 
+  binding.pry 
+  rebounds 
 end 
 
 
